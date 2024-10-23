f109619274ff93ff3c272437b69a15269deabe2e
Manika-Rajan <105493699+Manika-Rajan@users.noreply.github.com>
2024-10-23 10:29:05 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -189,7 +189,7 @@ function App() {
 
       {/* Updates Section */}
       <div style={{ 
-        marginTop: '10px', 
+        marginTop: '50px', 
         padding: '10px', 
         backgroundColor: '#f9f9f9', 
         borderTop: '1px solid #ccc' 
