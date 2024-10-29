5f10ce3f7d73fe986335ef1149e89d6e5bf4bb6b
Manika-Rajan <105493699+Manika-Rajan@users.noreply.github.com>
2024-10-29 12:28:26 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,7 @@ function App() {
     { updateDate: "2024-10-20", status: "Completed", updatesDone: "List of Super Markets in Chandanagar" },
     { updateDate: "2024-10-24", status: "Completed", updatesDone: "List of Shops/shutters for rent" },
     { updateDate: "2024-10-28", status: "In Progress", updatesDone: "List of Products generally held in a Super Market" },
-    { updateDate: "2024-11-01", status: "In Progress", updatesDone: "Products selling the most in Super Markets" },
+    { updateDate: "2024-11-01", status: "In Progress", updatesDone: "Products selling the most in Super Markets, List of Licenses and Registrations required" },
     { updateDate: "2024-11-05", status: "", updatesDone: "" },
     { updateDate: "2024-11-09", status: "", updatesDone: "" },
     { updateDate: "2024-11-13", status: "", updatesDone: "" },
