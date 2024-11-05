ddb4f8e0f7550f8a1462921b9c8490cf7d298fdc
Manika-Rajan <105493699+Manika-Rajan@users.noreply.github.com>
2024-11-05 14:57:17 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ Amplify.configure(outputs);
 ReactDOM.createRoot(document.getElementById("root")!).render(
     
   <React.StrictMode>
-    <Authenticator>
+    <Authenticator hideSignup={true}>
       <App />
     </Authenticator>
   </React.StrictMode>
