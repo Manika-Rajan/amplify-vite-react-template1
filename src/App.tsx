49d94721a41eb39078cd2ea3e525e568ba3a9454
Manika-Rajan <105493699+Manika-Rajan@users.noreply.github.com>
2024-11-14 13:33:29 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -441,7 +441,7 @@ function App() {
         {/* Final Report */}
         <div style={{ padding: '10px', backgroundColor: '#e6e6e6', borderRadius: '8px' }}>
           <h3>Final Report</h3>
-          <p><strong>Final Report :</strong> download link (work in progress).</p>
+          <p><strong>Final Report :</strong> <u>download-link</u> (work in progress).</p>
         </div>
       </div>
     </div>
